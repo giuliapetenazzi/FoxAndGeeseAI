ea4516e947ca72fbafec9fe0a4da5f2ab9faa216
Jordan <jordangottardo@libero.it>
2018-04-07 23:32:42 +0200

    iniziato alfabeta

---
+++ Assets/Scripts/Model/AI/AlfaBeta.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FoxAndGeese {
+	public class AlfaBeta {
+
+		//indica la mossa migliore da fare a partire 
+		//dallo stato da cui viene invocato AlfaBeta
+		private Move bestMove;
+		//indica chi è il giocatore controllato dalla IA
+		private PawnType aiPlayer;
+		
+		public AlfaBeta(PawnType aiPlayer) {
+			this.aiPlayer = aiPlayer;
+		}
+
+		//board indica lo stato di gioco
+		public Move AlphaBeta(Board board) {
+			MaxValue(board, int.MinValue, int.MaxValue);
+			return bestMove;
+		}
+
+		//Oche
+		private int MaxValue(Board board, int alpha, int beta) {
+			if (board.GameOver()) {
+				return board.EvaluateBoard(aiPlayer);
+			}
+			int v = int.MinValue;
+			List<Move> moves = board.GetPossibleMoves();
+
+			foreach (Move move in moves) {
+				Board deepCopiedBoard = board.GetDeepCopy();
+				deepCopiedBoard.Move(move); //serve cambiare il player attuale nella board
+				int score = MinValue(deepCopiedBoard, alpha, beta);
+				
+				if (score > alpha) {
+					alpha = score;
+					bestMove = move;
+				}
+			}
+			return v;
+		}
+
+		//Volpe
+		private int MinValue(Board board, int alpha, int beta) {
+			return 0;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Model/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FoxAndGeese {
+	//Rappresenta una board di gioco in un dato istante
+	public class Board {
+
+		public PawnType turn { get; private set; }
+		public PawnType[,] board { get; private set; }
+
+		public bool GameOver() {
+			return true;
+		}
+
+		public int EvaluateBoard(PawnType aiPlayer) {
+			return 0;
+		}
+
+		public List<Move> GetPossibleMoves() {
+			List<Move> moves = new List<Move>();
+			return moves;
+		}
+
+		public Board GetDeepCopied() {
+			return new Board();
+		}
+	}
+}
