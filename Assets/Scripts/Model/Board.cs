85b5d0f3fd0945e797d73e4c1bcc40f2cc4911c4
giuliapetenazzi <giuliaanno95@hotmail.it>
2018-04-10 10:45:12 +0200

    portato metodo dentro board da my utility

--- Assets/Scripts/Model/Board.cs
+++ Assets/Scripts/Model/Board.cs
@@ -3,40 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace FoxAndGeese {
-	//Rappresenta una board di gioco in un dato istante
-	public class Board {
+    //Rappresenta una board di gioco in un dato istante
+    public class Board {
 
-		public PawnType turn { get; private set; }
-		public PawnType[,] board { get; private set; }
+        public PawnType turn { get; private set; }
+        public PawnType[,] board { get; private set; }
 
-		public bool GameOver() {
-			return true;
-		}
+        public bool GameOver() {
+            return true;
+        }
 
-		public int EvaluateBoard(PawnType aiPlayer) {
-			return 0;
-		}
+        public int EvaluateBoard(PawnType aiPlayer) {
+            return 0;
+        }
 
-		public List<Move> GetPossibleMoves(PawnType current_player, int r, int c) {
-			List<Move> allPossibleMoves = new List<Move>();
-            /*
+        public List<Move> GetPossibleMoves(PawnType current_player, int r, int c) {
+            List<Move> allPossibleMoves = new List<Move>();
             if (current_player == PawnType.Fox) {
-                allPossibleMoves.AddRange(MyUtilityIA.IACalculateFoxValidMoves(r, c));
-                allPossibleMoves.AddRange(MyUtilityIA.IACalculateFoxValidEatingMoves(r, c));
+                allPossibleMoves.AddRange(IACalculateFoxValidMoves(r, c));
+                allPossibleMoves.AddRange(IACalculateFoxValidEatingMoves(r, c));
             }
             if (current_player == PawnType.Goose) {
-                allPossibleMoves.AddRange(MyUtilityIA.IACalculateGooseValidMoves(r, c));
+                allPossibleMoves.AddRange(IACalculateGooseValidMoves(r, c));
             }
-            */
-			return allPossibleMoves;
-		}
+            return allPossibleMoves;
+        }
 
-		public Board GetDeepCopy() {
-			return new Board();
-		}
-		
-		public void Move(Move move) {
-		
-		}
-	}
+        public Board GetDeepCopy() {
+            return new Board();
+        }
+
+        public void Move(Move move) {
+
+        }
+
+        //la goose non puo andare in diagonale
+        //non puo retrocedere di riga (anzi, cioò per gli indici avanzare)
+        //restano solo tre mosse: avanti, dx, sx
+        private List<Move> IACalculateGooseValidMoves(int r, int c) {
+            List<Move> gooseValidMoves = new List<Move>();
+            if (!MyUtility.IsPositionOutOfCross(r - 1, c) && board[r - 1, c] == PawnType.None)
+                gooseValidMoves.Add(new Move(PawnType.Goose, r, c, r - 1, c));
+            if (!MyUtility.IsPositionOutOfCross(r, c - 1) && board[r, c - 1] == PawnType.None)
+                gooseValidMoves.Add(new Move(PawnType.Goose, r, c, r, c - 1));
+            if (!MyUtility.IsPositionOutOfCross(r, c + 1) && board[r, c + 1] == PawnType.None)
+                gooseValidMoves.Add(new Move(PawnType.Goose, r, c, r, c + 1));
+            return gooseValidMoves;
+        }
+
+        //la fox si puo muovere in tutte le 8 direzioni, a distanza 1
+        private List<Move> IACalculateFoxValidMoves(int r, int c) {
+            List<Move> foxValidMoves = new List<Move>();
+            //setto l'intorno del centro dato
+            for (int roundR = r - 1; roundR <= r + 1; roundR++) {
+                for (int roundC = c - 1; roundC <= c + 1; roundC++) {
+                    if (!MyUtility.IsPositionOutOfCross(roundR, roundC) &&
+                        !(roundR == r && roundC == c) &&
+                        board[roundR, roundC] == PawnType.None) {
+                        foxValidMoves.Add(new Move(PawnType.Fox, r, c, roundR, roundC));
+                    }
+                }
+            }
+            return foxValidMoves;
+        }
+
+        //la fox per mangiare si puo muovere in tutte le 8 direzioni, a distanza 2
+        private List<Move> IACalculateFoxValidEatingMoves(int r, int c) {
+            List<Move> foxValidEatingMoves = new List<Move>();
+            for (int roundR = r - 2; roundR <= r + 2; roundR += 2) {
+                for (int roundC = c - 2; roundC <= c + 2; roundC += 2) {
+                    if (
+                        //la casella di destinazione deve essere nella board
+                        !MyUtility.IsPositionOutOfCross(roundR, roundC) &&
+                        //la casella interpolata deve essere nella board
+                        !MyUtility.IsPositionOutOfCross((roundR + r) / 2, (roundC + c) / 2) &&
+                        //non ci si può muovere verso la stessa casella di partenza
+                        !(roundR == r && roundC == c) &&
+                        //la casella di destinazione deve essere vuota
+                        board[roundR, roundC] == PawnType.None &&
+                        //la casella interpolata deve avere una goose sotto
+                        board[(roundR + r) / 2, (roundC + c) / 2] == PawnType.Goose) {
+                        foxValidEatingMoves.Add(new Move(PawnType.Fox, r, c, roundR, roundC));
+                    }
+                }
+            }
+            return foxValidEatingMoves;
+        }
+    }
 }
