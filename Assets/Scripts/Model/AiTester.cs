51e7b6bc9ff6ffe6f641b2eff660b386fa8126e7
Jordan <jordangottardo@libero.it>
2018-04-20 16:00:17 +0200

    .

--- Assets/Scripts/Model/AiTester.cs
+++ Assets/Scripts/Model/AiTester.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using FoxAndGeese;
 using System;
 
@@ -8,25 +9,26 @@ using System;
 public class AiTester {
 	static AiTester() {
 		Debug.Log("ciao");
-		Game game = new Game(15, true);
-		AlphaBeta player1 = new AlphaBeta(PawnType.Fox, 2);
-		AlphaBeta player2 = new AlphaBeta(PawnType.Goose, 2);
+		AlphaBeta player1 = new AlphaBeta(PawnType.Fox, 3);
+		AlphaBeta player2 = new AlphaBeta(PawnType.Goose, 3);
 		int counterTurn = 0;
 		Debug.Log("AiTester inizio");
-		while (!game.IsGameOver() && counterTurn < 4000) {
-			Debug.Log("turno" + counterTurn + "goose num = " +
-				game.GetGooseNumber());
-			Move move1 = player1.RunAlphaBeta(game);
-			game.MovePawn(move1);
-			if (game.IsGameOver()) {
-				break;
+		for (int i = 0; i < 10; i++) {
+			Game game = new Game(15, true);
+			while (!game.IsGameOver() && counterTurn < 850000) {
+				//Debug.Log("turno" + counterTurn + "goose num = " +
+				//game.GetGooseNumber());
+				Move move1 = player1.RunAlphaBeta(game);
+				game.MovePawn(move1);
+				if (game.IsGameOver()) {
+					break;
+				}
+				Move move2 = player2.RandomMove(game);
+				game.MovePawn(move2);
+				counterTurn++;
 			}
-			Move move2 = player2.RandomMove(game);
-			game.MovePawn(move2);
-			counterTurn++;
+
+			Debug.Log("AITESTER FINIO" + " TURNO = " + counterTurn + " VINCITORE = " + game.winner + " OCHE = " + game.GetGooseNumber());
 		}
-
-		//Debug.Log("AITESTER FINIO" + game.GetGooseNumber());
-
 	}
 }
