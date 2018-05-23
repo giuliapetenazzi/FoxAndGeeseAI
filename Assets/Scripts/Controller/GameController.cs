1a837c3703f86a83adccda16b1e75545122b3c57
Jordan <jordangottardo@libero.it>
2018-05-23 20:51:39 +0200

    solo number of goose

--- Assets/Scripts/Controller/GameController.cs
+++ Assets/Scripts/Controller/GameController.cs
@@ -51,7 +51,7 @@ public class GameController : StateMachine {
 		ClearBoard();
 		mainCamera.GetComponent<MoveCamera>().PositionCamera(humanPlayer);
 		game = new Game(15, false);
-		alphaBeta = new AlphaBeta(cpuPlayer, 5, new WeightsForBoardEval(2,2), false);
+		alphaBeta = new AlphaBeta(cpuPlayer, 6, new WeightsForBoardEval(2, 0, 0, 0, 0, 0, 0), false);
 		CheckState();
 		if (game.turn == cpuPlayer) {
 			AiMoves();
